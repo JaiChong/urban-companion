5359a13991958a2e98540a184d27192dc7d0a0ad
JaiChong <jaimi.chong@gmail.com>
2024-02-03 20:33:05 -0800

    p2: exp backoff, prints

--- MyCity.java
+++ MyCity.java
@@ -21,6 +21,7 @@
 import java.io.Console;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.Thread;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.Redirect;
 import java.net.http.HttpRequest;
@@ -34,17 +35,20 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+
 public class MyCity
 {
   public static void main(String[] args)
   {
     // 1. PROGRAM SETUP
     process_args(args);
+    print_intro();
 
     // 2. API HANDLING
-    if (http_call(URL_API_WEATHER))     parse_print_resp("weather");
-    if (http_call(URL_API_HOTELS))      parse_print_resp("hotels");
-    if (http_call(URL_API_RESTAURANTS)) parse_print_resp("restaurants");
+    if (http_call(API_1_NAM, API_1_URL)) parse_resp_append_results(1);
+    if (http_call(API_2_NAM, API_2_URL)) parse_resp_append_results(2);
+    if (http_call(API_3_NAM, API_3_URL)) parse_resp_append_results(3);
+    System.out.println(results);
   }
 
 
@@ -59,30 +63,39 @@ public class MyCity
   // Program args
   private static String secret;
   public  static String city;
-  
-  // API URLs
-  public static final String URL_API_WEATHER     = "http://api.openweather.org/data/2.5/weather?q=&appid=";
-  public static final String URL_API_HOTELS      = "";
-  public static final String URL_API_RESTAURANTS = "";
+
+  // APIs
+  public static final String API_1_NAM = "OpenWeatherMap";
+  public static final String API_1_URL = "http://api.openweather.org/data/2.5/weather?q=&appid=";
+  public static final String API_2_NAM = "";
+  public static final String API_2_URL = "";
+  public static final String API_3_NAM = "";
+  public static final String API_3_URL = "";
 
   // HTTP calls
   public static HttpClient           client;
   public static HttpRequest          req;
   public static HttpResponse<String> resp;
   public static int                  resp_status;
-  
+  public static final int RETRY_BASE = 2;
+  public static final int RETRY_MAX  = 4;
+
+  // Results
+  public static String results = "\n\n//============" +
+                                     "// RESULTS //" +
+                                     "//==========";
 
   //=============================
   // 0.2. HTTP Call Functions //
   //===========================
   
-  public static boolean http_call(String url)
+  public static boolean http_call(String nam, String url)
   {
-    // System.out.println("\nCalling " + api_name + " ...");
+    System.out.println("\nCalling " + nam + "...");
     client  = HttpClient.newBuilder().followRedirects(Redirect.ALWAYS).build();
     req     = HttpRequest.newBuilder().uri(URI.create(url + city + "&appid=" + secret)).build();
     update_resp_exp_backoff();
-    return resp_status >= 200 && resp_status < 400;
+    return resp_status < 400;
   }
 
   public static void update_resp_exp_backoff()
@@ -90,17 +103,30 @@ public class MyCity
     // client.send() -> Exceptions
     try
     {
-      // retries Server Errors once
-      int retries = 0;
-      do
+      // for (4 retries or until success)
+      for (int retry = 0; retry <= RETRY_MAX; retry++)
       {
         resp = client.send(req, BodyHandlers.ofString());
         resp_status = resp.statusCode();
-        if (resp_status >= 500) retries++;
+        System.out.print(" Status "+resp_status+": ");
+        if (resp_status < 400)
+        {
+          System.out.println("Success!");
+          retry = RETRY_MAX;                            // exit loop
+        }
+        else
+        {
+          System.out.print("Failure");
+          if (retry < RETRY_MAX && (resp_status >= 500 || resp_status == 429))
+          {
+            System.out.println(", retrying in "+(RETRY_BASE ^ retry)+" seconds ...");
+            Thread.sleep(1000 * (RETRY_BASE ^ retry));  // 1000ms*(2^n) -> 1s, 2s, 4s, 8s, give up
+          }
+          else System.out.println(".");
+        }
       }
-      while (retries == 1);
     }
-    catch (IOException | InterruptedException e) { System.out.println(e.getMessage()); System.exit(1); }
+    catch (IOException | InterruptedException e) { System.out.println("\n" + e.getMessage()); System.exit(1); }
   }
 
 
@@ -122,25 +148,54 @@ public class MyCity
     city          = args[1];
   }
 
+  public static void print_intro()
+  {
+    System.out.println("\n//==========");
+    System.out.println(  "// INTRO //");
+    System.out.println(  "//========");
 
-  //==================
-  // 2. API HANDLING ||
-  //==================
+    System.out.println("\nMyCity.java");
+    System.out.println(  " Input:  City name");
+    System.out.println(  " Output: Local info gathered from three independent API");
+    System.out.println(  " Algo:   Exponential Backoff (delay between calls increases exponentially)");
+    System.out.println(  " Consts: RETRY_BASE="+RETRY_BASE+", RETRY_MAX="+RETRY_MAX+", three URL_API_*'s");
 
-  public static void parse_print_resp(String api_type)
+    System.out.println("\nHTTP Response Status Codes Key:");
+    System.out.println(  " Accepted: 100s=Informtional,  200s=Success,  300s=Redirects");
+    System.out.println(  " Rejected: 400s=ClientErr");
+    System.out.println(  " Retried:   429=TooManyReqs,   500s=ServerErr");
+
+
+    System.out.println("\n\n//==============");
+    System.out.println(    "// API CALLS //");
+    System.out.println(    "//============");
+  }
+
+
+  //===========================
+  // 2. API RESPONSE HANDLING ||
+  //===========================
+
+  public static void parse_resp_append_results(int api_num)
   {
     // RootObject weatherDetails = JsonConvert.DeserializeObject<RootObject>(response)!;
-    switch (api_type)
+    switch (api_num)
     {
-      case "weather":
+      case 1:
         // int todayTemp = (int) weatherDetails.main.temp;
         // if (todayTemp > 55) Console.WriteLine("surprisingly warm today");
+        results += "\n"+API_1_NAM+":" +
+                       " info";
         break;
 
-      case "hotels":
+      case 2:
+        results += "\n"+API_2_NAM+":" +
+                       " info";
         break;
       
-      case "restaurants":
+      case 3:
+        results += "\n"+API_2_NAM+":" +
+                       " info";
         break;
     }
   }
